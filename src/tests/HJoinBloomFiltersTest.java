7bbceb7ee8e8c9643f07704abdf122ed84e6d85a
Mark Gray <mmg3339@cs.utexas.edu>
2014-11-25 22:36:06 -0600

    added Hjoin bloom filters test

--- src/tests/HJoinBloomFiltersTest.java
+++ src/tests/HJoinBloomFiltersTest.java
@@ -1,16 +1,38 @@
 package tests;
 
-import static org.junit.Assert.*;
+import gammaIntegration.HJoinBloomFilters;
+import gammaSupport.ThreadList;
+import gammajoins.Print;
+import gammajoins.ReadRelation;
 
 import org.junit.Test;
+
+import RegTest.Utility;
+import basicConnector.Connector;
 
 public class HJoinBloomFiltersTest {
 
 	@Test
-	public void HJoinBloomFiltersTest1() {
+	public void HJoinBloomFiltersTest1() throws Exception {
+		Utility.redirectStdOut("outputFiles/hjoinBFOut");
+		HJoin("tables/client.txt", "tables/viewing.txt", 0, 0);
+		System.out.println();
+		HJoin("tables/orders.txt", "tables/odetails.txt", 0, 0);
+		System.out.println();
+		HJoin("tables/parts.txt", "tables/odetails.txt", 0, 1);
+		Utility.validate("outputFiles/hjoinBFOut", "correctOutput/hjoinBFCOutput", true);
+		
 	}
 	
-	public void HJoin (String fileNameA, String fileNameB, int aJoinKey, int bJoinKey) {
-		
+	public void HJoin (String fileNameA, String fileNameB, int aJoinKey, int bJoinKey) throws Exception {
+		ThreadList.init();
+		Connector readInA = new Connector("readRtoBloomA");
+		Connector readInB = new Connector("readRtoBloomB");
+		Connector output = new Connector("output");
+		ReadRelation rRelationA = new ReadRelation(fileNameA, readInA);
+		ReadRelation rRelationB = new ReadRelation(fileNameB, readInB);
+		HJoinBloomFilters hJoinBF = new HJoinBloomFilters(readInA, readInB, output, aJoinKey, bJoinKey);
+		Print print = new Print(output);
+		ThreadList.run(print);
 	}
 }
