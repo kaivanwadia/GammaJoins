9f9b6d06cc1ff17394a5bff5826f0af80d318d41
Mark Gray <mmg3339@cs.utexas.edu>
2014-11-25 23:09:03 -0600

    Test for MRHJoin and MRBFilter

---
+++ src/tests/MapReduceBFilterTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import gammaIntegration.MapReduceBFilter;
+import gammaSupport.ThreadList;
+import gammajoins.Bloom;
+import gammajoins.Print;
+import gammajoins.ReadRelation;
+
+import org.junit.Test;
+
+import RegTest.Utility;
+import basicConnector.Connector;
+
+public class MapReduceBFilterTest {
+	@Test
+	public void MapReduceBFilterTest() throws Exception {
+		Utility.redirectStdOut("outputFiles/mapReduceBFOut");
+		mapReduceBF("tables/client.txt", "tables/viewing.txt", 0, 0);
+		System.out.println();
+		mapReduceBF("tables/orders.txt", "tables/odetails.txt", 0, 0);
+		System.out.println();
+		mapReduceBF("tables/parts.txt", "tables/odetails.txt", 0, 1);
+		Utility.validate("outputFiles/mapReduceBFOut", "correctOutput/mapReduceBFCOutput", true);
+		
+	}
+	
+	public void mapReduceBF (String fileNameA, String fileNameB, int aJoinKey, int bJoinKey) throws Exception {
+		ThreadList.init();
+		Connector readInA = new Connector("readMapreduceBFilterA");
+		Connector readInB = new Connector("readMapreduceBFilterB");
+		Connector output = new Connector("output");
+		ReadRelation rRelationA = new ReadRelation(fileNameA, readInA);
+		ReadRelation rRelationB = new ReadRelation(fileNameB, readInB);
+		Connector bloomOut = new Connector("mergeMapA");
+		Connector bloomAMap = new Connector("mmergeMapA");
+		Bloom bloomA = new Bloom(readInA, bloomOut, bloomAMap, aJoinKey);
+		MapReduceBFilter mapReduceBFilter = new MapReduceBFilter(bloomAMap, readInB, bJoinKey, output);
+		Print print = new Print(output);
+		ThreadList.run(print);
+	}
+}
---
+++ src/tests/MapReduceHJoinTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import gammaIntegration.MapReduceHJoin;
+import gammaSupport.ThreadList;
+import gammajoins.Print;
+import gammajoins.ReadRelation;
+
+import org.junit.Test;
+
+import RegTest.Utility;
+import basicConnector.Connector;
+
+public class MapReduceHJoinTest {
+
+	@Test
+	public void mapReduceHJoin() throws Exception {
+		Utility.redirectStdOut("outputFiles/mapReduceHJoinOut");
+		mrHJoin("tables/client.txt", "tables/viewing.txt", 0, 0);
+		System.out.println();
+		mrHJoin("tables/orders.txt", "tables/odetails.txt", 0, 0);
+		System.out.println();
+		mrHJoin("tables/parts.txt", "tables/odetails.txt", 0, 1);
+		Utility.validate("outputFiles/mapReduceHJoinOut", "correctOutput/mapReduceHJoinCOutput", true);
+	}
+	
+	public void mrHJoin (String fileNameA, String fileNameB, int aJoinKey, int bJoinKey) throws Exception {
+		ThreadList.init();
+		System.out.println("Map Reduce HJoin OF " + fileNameA + " and " + fileNameB);
+		Connector aToMRHJoin = new Connector("aToMRHJoin");
+		Connector bToMRHJoin = new Connector("bToMRHJoin");
+		Connector output = new Connector("output");
+		ReadRelation readA = new ReadRelation(fileNameA, aToMRHJoin);
+		ReadRelation readB = new ReadRelation(fileNameB, bToMRHJoin);
+		MapReduceHJoin gamma = new MapReduceHJoin (aToMRHJoin, bToMRHJoin, output, aJoinKey, bJoinKey);
+		Print print = new Print(output);
+		ThreadList.run(print);
+	}
+	
+}
