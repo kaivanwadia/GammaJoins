54371fb9c76efec901a02df9711e58ccd97a9e7e
MarkMGray <markmgray2010@gmail.com>
2014-11-25 19:16:19 -0600

    Added HJoin Parrallelization

--- src/gammaIntegration/HJoinRefineWithBloomFilters.java
+++ src/gammaIntegration/HJoinRefineWithBloomFilters.java
@@ -1,5 +1,31 @@
 package gammaIntegration;
 
-public class HJoinRefineWithBloomFilters {
+import gammaSupport.ArrayConnectors;
+import gammaSupport.ThreadList;
+import gammajoins.Bloom;
+import gammajoins.HSplit;
+import gammajoins.Merge;
+import gammajoins.MergeM;
+import basicConnector.Connector;
 
+public class HJoinRefineWithBloomFilters extends ArrayConnectors{
+	
+	public HJoinRefineWithBloomFilters(Connector in1, int joinKey, Connector mergeOut, Connector mmergeOut){
+		
+		Connector[] outs = ArrayConnectors.initConnectorArray("out");
+		Connector[] bloomT = ArrayConnectors.initConnectorArray("bloomT");
+		Connector[] bloomM = ArrayConnectors.initConnectorArray("bloomM");
+		
+		
+		HSplit hSplit = new HSplit(in1, outs[0], outs[1], outs[2], outs[3], joinKey);
+		
+		Bloom b1 = new Bloom(outs[0], bloomT[0], bloomM[0], joinKey);
+		Bloom b2 = new Bloom(outs[1], bloomT[1], bloomM[1], joinKey);
+		Bloom b3 = new Bloom(outs[2], bloomT[2], bloomM[2], joinKey);
+		Bloom b4 = new Bloom(outs[3], bloomT[3], bloomM[3], joinKey);
+		
+		Merge merge = new Merge(bloomT[0], bloomT[1], bloomT[2], bloomT[3], mergeOut);
+		MergeM mmerge = new MergeM(bloomM[0], bloomM[1], bloomM[2], bloomM[3], mmergeOut);
+		
+	}
 }
--- src/gammajoins/Main.java
+++ src/gammajoins/Main.java
@@ -1,7 +1,7 @@
 package gammajoins;
 
+import gammaIntegration.HJoinRefineWithBloomFilters;
 import gammaSupport.ThreadList;
-import RegTest.Utility;
 import basicConnector.Connector;
 
 public class Main {
@@ -9,21 +9,27 @@ public class Main {
 		String fileName = "tables/client.txt";
 		int joinKey = 0;
 		ThreadList.init();
-		Connector readRToBloom = new Connector("readRToBloom");
-		Connector bloomToSinkTuple = new Connector("bloomToSinkTuple");
-		Connector bloomToSplitMap = new Connector("bloomBmapout1");
-		Connector splitMToMerge1 = new Connector("splitMToMerge1");
-		Connector splitMToMerge2 = new Connector("splitMToMerge2");
-		Connector splitMToMerge3 = new Connector("splitMToMerge3");
-		Connector splitMToMerge4 = new Connector("splitMToMerge4");
-		Connector mergeMToPrint = new Connector("mergeMToPrint");
-		ReadRelation rRelation = new ReadRelation(fileName, readRToBloom);
-		Bloom bloom1 = new Bloom(readRToBloom, bloomToSinkTuple, bloomToSplitMap, 0);
-		SplitM splitM = new SplitM(bloomToSplitMap, splitMToMerge1, splitMToMerge2, splitMToMerge3, splitMToMerge4);
-		MergeM mergeM = new MergeM(splitMToMerge1, splitMToMerge2, splitMToMerge3, splitMToMerge4, mergeMToPrint);
-		Sink sinkTuples = new Sink(bloomToSinkTuple);
-		PrintMap printMap = new PrintMap(mergeMToPrint);
-//		Print print = new Print(bloomToSinkTuple);
-		ThreadList.run(printMap);
+		Connector readIn = new Connector("readRtoBloom");
+		Connector mergeOut = new Connector("bloomMergeOut");
+		Connector mmergeOut = new Connector("bloomMMergeOut");
+		ReadRelation rRelation = new ReadRelation(fileName, readIn);
+		HJoinRefineWithBloomFilters hjf = new HJoinRefineWithBloomFilters(readIn, joinKey, mergeOut, mmergeOut);
+		
+//		Connector readRToBloom = new Connector("readRToBloom");
+//		Connector bloomToSinkTuple = new Connector("bloomToSinkTuple");
+//		Connector bloomToSplitMap = new Connector("bloomBmapout1");
+//		Connector splitMToMerge1 = new Connector("splitMToMerge1");
+//		Connector splitMToMerge2 = new Connector("splitMToMerge2");
+//		Connector splitMToMerge3 = new Connector("splitMToMerge3");
+//		Connector splitMToMerge4 = new Connector("splitMToMerge4");
+//		Connector mergeMToPrint = new Connector("mergeMToPrint");
+//		ReadRelation rRelation = new ReadRelation(fileName, readRToBloom);
+//		Bloom bloom1 = new Bloom(readRToBloom, bloomToSinkTuple, bloomToSplitMap, 0);
+//		SplitM splitM = new SplitM(bloomToSplitMap, splitMToMerge1, splitMToMerge2, splitMToMerge3, splitMToMerge4);
+//		MergeM mergeM = new MergeM(splitMToMerge1, splitMToMerge2, splitMToMerge3, splitMToMerge4, mergeMToPrint);
+//		Sink sinkTuples = new Sink(bloomToSinkTuple);
+		PrintMap printMap = new PrintMap(mmergeOut);
+		Print print = new Print(mergeOut);
+		ThreadList.run(print);
 	}
 }
